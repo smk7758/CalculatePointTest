43985ab75934509350b6ae4de8c23068883ddd06
smk7758 <maxkovko@yahoo.co.jp>
2019-06-04 23:13:54 +0900

    fixing theory.
    理論の結構な修正。
    無駄な行の削除。

--- src/com/github/smk7758/GetSubstituteFingerPoint/CalculatePoint.java
+++ src/com/github/smk7758/GetSubstituteFingerPoint/CalculatePoint.java
@@ -42,32 +42,35 @@ public class CalculatePoint {
 		Calib3d.Rodrigues(rotationVector, rotationMatrix);
 		Main.debugLog("R: " + rotationMatrix.dump(), LogLevel.DEBUG, "process | CalculatePoint");
 
-		Mat translationVector_ = convertTranslationVector(translationVector);
+		Mat translationVector_ = convertVerticalTranslationVectorHorizontal(translationVector);
 
 		// A_, 指先の点(Z=f)
 		Mat vectorA_ = new Mat(3, 1, CvType.CV_64F);
-		vectorA_.put(0, 0, new double[] { fingerPoint.x, fingerPoint.y, focus });
+		// vectorA_.put(0, 0, new double[] { focus, fingerPoint.y, fingerPoint.x }); //TODO
+
 		Main.debugLog("vectorA_: " + vectorA_.dump(), LogLevel.DEBUG, "process | CalculatePoint");
 
 		// create Nw
 		Mat vectorNw = new Mat(3, 1, CvType.CV_64F);
-		vectorNw.put(0, 0, new double[] { 0, 0, 1 });
+		vectorNw.put(0, 0, new double[] { 0, 1, 0 });
 
 		// Nw → N
 		Mat vectorN = new Mat();
-		CalculatePoint.convertToWorldCoordinate(vectorNw, rotationMatrix, translationVector_, vectorN);
+		// CalculatePoint.convertToWorldCoordinate(vectorNw, rotationMatrix, translationVector_, vectorN);
+		// vectorN = normalizeVector(vectorN);
+		Main.debugLog("vectorN: " + vectorN.dump(), LogLevel.DEBUG, "process | CalculatePoint");
 
-		double k_0 = vectorNw.dot(translationVector);
-		double k_1 = vectorNw.dot(vectorA_);
-		double k = vectorNw.dot(translationVector) / vectorNw.dot(vectorA_);
+		double k_0 = vectorN.dot(translationVector);
+		double k_1 = vectorN.dot(vectorA_);
+		double k = vectorN.dot(translationVector) / vectorN.dot(vectorA_);
 
-		Main.debugLog("k_0:  " + k_0 + ", k_1: " + k_1 + ", " + k, LogLevel.DEBUG, "process | CalculatePoint"); // TODO
+		Main.debugLog("k_0:  " + k_0 + ", k_1: " + k_1 + ", k: " + k, LogLevel.DEBUG, "process | CalculatePoint"); // TODO
 
-		Mat vectorA = new Mat(3, 1, CvType.CV_64F);
-		vectorA = vectorA_.mul(Mat.ones(vectorA_.size(), vectorA_.type()), k); // TODO
+		// Mat vectorA = new Mat(3, 1, CvType.CV_64F);
+		Mat vectorA = vectorA_.mul(Mat.ones(vectorA_.size(), vectorA_.type()), k);
 
-		Main.debugLog("A:  " + vectorA.dump(), LogLevel.DEBUG, "process | CalculatePoint");
 		Main.debugLog("A_: " + vectorA_.dump(), LogLevel.DEBUG, "process | CalculatePoint");
+		Main.debugLog("A:  " + vectorA.dump(), LogLevel.DEBUG, "process | CalculatePoint");
 
 		Mat vectorAw = new Mat(3, 1, CvType.CV_64F);
 
@@ -97,9 +100,82 @@ public class CalculatePoint {
 		// System.out.println(pointsDst.dump()); // TODD
 	}
 
+	/**
+	 * 指先の点の空間座標を変換して、接地判定を行う。(改良版？)
+	 *
+	 * @param vectorA_ 指先の点(Z=f)
+	 */
+	public void process_(Mat vectorA_, Mat rotationMatrix, Mat translationVector, Mat outputMat) {
+		// Main.debugLog("Rv: " + rotationVector.dump(), LogLevel.DEBUG, "process | CalculatePoint");
+
+		// Mat rotationMatrix = new Mat();
+		// Calib3d.Rodrigues(rotationVector, rotationMatrix);
+		Main.debugLog("R: " + rotationMatrix.dump(), LogLevel.DEBUG, "process | CalculatePoint");
+
+		// Mat translationVector_ = convertVerticalTranslationVectorHorizontal(translationVector);
+		Mat translationVector_ = translationVector;
+
+		// A_, 指先の点(Z=f)
+		// Mat vectorA_ = new Mat(3, 1, CvType.CV_64F);
+		// vectorA_.put(0, 0, new double[] { focus, fingerPoint.y, fingerPoint.x }); //TODO
+
+		Main.debugLog("vectorA_: " + vectorA_.dump(), LogLevel.DEBUG, "process | CalculatePoint");
+
+		// create Nw
+		Mat vectorNw = new Mat(3, 1, CvType.CV_64F);
+		vectorNw.put(0, 0, new double[] { 0, 1, 0 });
+
+		// Nw → N
+		Mat vectorN = new Mat();
+
+		// Nwベクトルの座標軸的な角度の変換 N = R*Nw
+		convertToWorldCoordinate(vectorNw, rotationMatrix, new Mat(), vectorN);
+
+		double k_0 = vectorN.dot(translationVector);
+		double k_1 = vectorN.dot(vectorA_);
+		double k = vectorN.dot(translationVector) / vectorN.dot(vectorA_);
+
+		Main.debugLog("k_0:  " + k_0 + ", k_1: " + k_1 + ", k: " + k, LogLevel.DEBUG, "process | CalculatePoint"); // TODO
+
+		Mat vectorA = vectorA_.mul(Mat.ones(vectorA_.size(), vectorA_.type()), k);
+
+		Main.debugLog("A:  " + vectorA.dump(), LogLevel.DEBUG, "process | CalculatePoint");
+		Main.debugLog("A_: " + vectorA_.dump(), LogLevel.DEBUG, "process | CalculatePoint");
+
+		Mat vectorAw = new Mat(3, 1, CvType.CV_64F);
+
+		CalculatePoint.unconvertToWorldCoordinate_(vectorA, rotationMatrix, translationVector_, vectorAw);
+
+		Main.debugLog("Aw: " + vectorAw.dump(), LogLevel.DEBUG, "process | CalculatePoint"); // TODO
+
+		int l = 5; // TODO
+		Mat vectorBw = new Mat();
+		Mat vectorNw_l = vectorNw.mul(Mat.ones(vectorNw.size(), vectorNw.type()), l);
+		Core.add(vectorAw, vectorNw_l, vectorBw);
+
+		Main.debugLog("vecrotNw_l: " + vectorNw_l.dump(), LogLevel.DEBUG, "process | CalculatePoint");
+
+		Main.debugLog("Bw: " + vectorBw.dump(), LogLevel.DEBUG, "process | CalculatePoint");
+
+		Point3 pointAw = new Point3(vectorAw.get(0, 0)[0], vectorAw.get(1, 0)[0], vectorAw.get(2, 0)[0]);
+		Point3 pointBw = new Point3(vectorBw.get(0, 0)[0], vectorBw.get(1, 0)[0], vectorBw.get(2, 0)[0]);
+		MatOfPoint3f pointsSrc = new MatOfPoint3f(pointAw, pointBw);
+		MatOfPoint2f pointsDst = new MatOfPoint2f();
+		Calib3d.projectPoints(pointsSrc, rotationMatrix, translationVector,
+				cameraMatrix, distortionCoefficients, pointsDst);
+
+		Main.debugLog("pointDst: " + pointsDst.dump(), LogLevel.DEBUG);
+
+		// Imgproc.circle(outputMat, new Point(pointsDst.get(0, 0)), 10, new Scalar(255, 255, 255));
+		// System.out.println(pointsDst.dump()); // TODD
+	}
+
+	/**
+	 * Word座標への変換をする cf: matDst = R*matSrc + t
+	 */
 	public static void convertToWorldCoordinate(Mat matSrc, Mat rotationMatrix, Mat translationVector, Mat matDst) {
-		Main.debugLog("matSrc: " + matSrc.dump(), LogLevel.DEBUG, "convertToWorldCoordinate");
-		Main.debugLog("t: " + translationVector.t().dump(), LogLevel.DEBUG, "convertToWorldCoordinate");
+		// Main.debugLog("matSrc: " + matSrc.dump(), LogLevel.DEBUG, "convertToWorldCoordinate");
+		// Main.debugLog("t: " + translationVector.t().dump(), LogLevel.DEBUG, "convertToWorldCoordinate");
 
 		Core.gemm(rotationMatrix, matSrc, 1, translationVector.t(), 1, matDst);
 
@@ -108,28 +184,37 @@ public class CalculatePoint {
 		// Core.add(matDst, translationVector, matDst);
 	}
 
+	/**
+	 * @param matSrc
+	 * @param rotationVector
+	 * @param translationVector
+	 * @param matDst
+	 */
 	public static void unconvertToWorldCoordinate(Mat matSrc, Mat rotationVector, Mat translationVector, Mat matDst) {
-		Main.debugLog("matSrc: " + matSrc.dump(), LogLevel.DEBUG, "convertToWorldCoordinate");
-		Main.debugLog("R: " + rotationVector.dump(), LogLevel.DEBUG, "convertToWorldCoordinate");
-		Main.debugLog("t: " + translationVector.t().dump(), LogLevel.DEBUG, "convertToWorldCoordinate");
+		// Main.debugLog("matSrc: " + matSrc.dump(), LogLevel.DEBUG, "convertToWorldCoordinate");
+		// Main.debugLog("R: " + rotationVector.dump(), LogLevel.DEBUG, "convertToWorldCoordinate");
+		// Main.debugLog("t: " + translationVector.t().dump(), LogLevel.DEBUG, "convertToWorldCoordinate");
 
 		// Mat rotationMatrix = new Mat();
-
-		// Core.invert(rotationMatrix, rotationMatrix_0);
+		// Core.invert(rotationMatrix, rotationMatrix_0); //回転行列の逆行列はこれでも良い。
 		// Imgproc.invertAffineTransform(rotationMatrix_0, rotationMatrix_0); // ゴミ
 
+		// Rodrigues行列の反転 (逆変換のため)
 		Mat rotationVector_0 = rotationVector.mul(Mat.eye(rotationVector.size(), rotationVector.type()), -1);
 
+		// 回転行列の逆行列をRodriges行列の反転したものから生成
 		Mat rotationMatrix_0 = new Mat();
 		Calib3d.Rodrigues(rotationVector_0, rotationMatrix_0);
 		Main.debugLog("R0: " + rotationMatrix_0.dump(), LogLevel.DEBUG, "convertToWorldCoordinate");
 
+		// tベクトルを転置して引き算して出力？(転置しないとならない理由は何？)
 		Core.subtract(matSrc, translationVector.t(), matDst);
 
 		// System.out.println("Rv0: " + rotationVector_0.dump() + ", " + rotationVector_0.height());
 
 		Main.debugLog("matDst(before, multi): " + matDst.dump(), LogLevel.DEBUG, "convertToWorldCoordinate");
 
+		// matDstは再利用物
 		Core.gemm(rotationMatrix_0, matDst, 1, new Mat(), 0, matDst);
 		// ImgProcessUtil.multiplicationMat(rotationVector_0, matDst, matDst);
 		// Core.multiply(rotationMatrix_0, matDst, matDst); // TODO
@@ -138,16 +223,46 @@ public class CalculatePoint {
 	}
 
 	/**
-	 * Mat型のtranslationVectorの横ベクトルを返す。
-	 *
+	 * @param matSrc
+	 * @param rotationVector
 	 * @param translationVector
+	 * @param matDst
+	 */
+	public static void unconvertToWorldCoordinate_(Mat matSrc, Mat rotationMatrix, Mat translationVector, Mat matDst) {
+		// Main.debugLog("matSrc: " + matSrc.dump(), LogLevel.DEBUG, "convertToWorldCoordinate");
+		// Main.debugLog("t: " + translationVector.t().dump(), LogLevel.DEBUG, "convertToWorldCoordinate");
+
+		Mat rotationMatrix_invert = rotationMatrix.inv(); // 逆行列だと思う
+
+		Main.debugLog("R_inv: " + rotationMatrix_invert.dump(), LogLevel.DEBUG, "convertToWorldCoordinate");
+
+		// tベクトルを転置して引き算して出力？(転置しないとならない理由は何？)
+		Core.subtract(matSrc, translationVector.t(), matDst);
+
+		// matDstは再利用物
+		Core.gemm(rotationMatrix_invert, matDst, 1, new Mat(), 0, matDst);
+	}
+
+	/**
+	 * 縦ベクトル(Vertical)のtranslationVectorの横ベクトル(Horizontal)を返す。← それ転置では？
+	 *
+	 * @param verticalTranslationVector
 	 * @return
 	 */
-	private static Mat convertTranslationVector(Mat translationVector) {
-		Mat translationVector_ = new Mat(1, 3, CvType.CV_64F);
-		for (int channel = 0; channel < translationVector.channels(); channel++) {
-			translationVector_.put(0, channel, translationVector.get(0, 0)[channel]);
+	private static Mat convertVerticalTranslationVectorHorizontal(Mat verticalTranslationVector) {
+		Mat horizontalTranslationVector = new Mat(1, 3, CvType.CV_64F);
+		for (int channel = 0; channel < verticalTranslationVector.channels(); channel++) {
+			horizontalTranslationVector.put(0, channel, verticalTranslationVector.get(0, 0)[channel]);
 		}
-		return translationVector_;
+		return horizontalTranslationVector;
+	}
+
+	/**
+	 * 単位ベクトル(unit vector)を返します。
+	 */
+	public static Mat normalizeVector(Mat vector) {
+		Mat dst = new Mat(vector.size(), vector.type());
+		Core.normalize(vector, dst, 1.0, 0, Core.NORM_L2);
+		return dst;
 	}
 }
--- src/com/github/smk7758/GetSubstituteFingerPoint/Main.java
+++ src/com/github/smk7758/GetSubstituteFingerPoint/Main.java
@@ -2,16 +2,14 @@ package com.github.smk7758.GetSubstituteFingerPoint;
 
 import java.nio.file.Paths;
 
-import org.opencv.calib3d.Calib3d;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.Point;
 import org.opencv.core.Size;
 
 public class Main {
 	private static final boolean DEBUG_MODE = true;
-	private final String cpPathString = "S:\\FingerPencil\\CalclatePoint_Test_2019-05-18\\CameraCalibration_Test_2019-05-18.xml";
+	private final String camparaPathString = "S:\\FingerPencil\\CalclatePoint_Test_2019-05-18\\CameraCalibration_Test_2019-05-18.xml";
 
 	static {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -34,26 +32,30 @@ public class Main {
 	}
 
 	public void processer() {
-		CameraParameter cameraParameter = new CameraParameter(Paths.get(cpPathString));
+		CameraParameter cameraParameter = new CameraParameter(Paths.get(camparaPathString));
 		CalculatePoint calculatePoint = new CalculatePoint(cameraParameter);
 
-		Point fingerPoint = new Point(1, 2);
+		// Point fingerPoint = new Point(0, 0);
+
+		Mat vectorA_ = new Mat(3, 1, CvType.CV_64F);
+		vectorA_.put(0, 0, new double[] { 0, 2, 1 });
+
 		Mat rotationMatrix = Mat.eye(new Size(3, 3), CvType.CV_64FC1);
+		rotationMatrix.put(1, 1, new double[] { -1 });
+
 		debugLog(rotationMatrix.dump(), LogLevel.DEBUG);
-		Mat rotationVector = new Mat();
-		{
-			Calib3d.Rodrigues(rotationMatrix, rotationVector);
-		}
+
 		Mat translationVector = new Mat(new Size(3, 1), CvType.CV_64FC1);
 		{
 			translationVector.put(0, 0, new double[] { 0 });
-			translationVector.put(0, 1, new double[] { 0 });
-			translationVector.put(0, 2, new double[] { 10 });
+			translationVector.put(0, 1, new double[] { 10 });
+			translationVector.put(0, 2, new double[] { 1 });
 		}
 		debugLog(translationVector.dump(), LogLevel.DEBUG);
-		Mat outputMat = new Mat();
-		calculatePoint.process(fingerPoint, rotationVector, translationVector, outputMat);
 
+		Mat outputMat = new Mat();
+
+		calculatePoint.process_(vectorA_, rotationMatrix, translationVector, outputMat);
 	}
 
 }
